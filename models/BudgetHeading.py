1728cf983b1aa86f930cfc0eb657b84e0924f996
Javier Molinero <100346219@alumnos.uc3m.es>
2017-02-22 11:26:12 +0100

    new class created

---
+++ models/BudgetHeading.py
@@ -0,0 +1,16 @@
+from .connection import db
+
+class BudgetHeading(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=1)
+    name = db.Column(db.String(255), nullable=False)
+    initial_amout=db.Column(db.Integer, nullable=False)
+    expenses=db.relationship('Expense', backref='budgetHeading', lazy='dynamic')
+    budget_id = db.Column(db.Integer, db.ForeignKey('budget.id'))
+
+    def __init__(self, name=None, initial_amout=None, expenses=[]):
+        self.name=name
+        self.initial_amout=initial_amout
+        self.expenses=expenses
+
+    def __repr__(self):
+        return{'id':self.id, 'name': self.name, 'initial_amoumt': self.initial_amout, 'expenses': self.expenses}
