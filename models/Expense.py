038c98075580c519ee58fc7f452b8a4972650b1d
David Marcos! <davidmarcosg98@gmail.com>
2017-02-27 20:12:02 +0100

    Modified Expense.py

--- models/Expense.py
+++ models/Expense.py
@@ -1,7 +1,6 @@
 from .connection import db
 
 class Expense(db.Model):
-
     id = db.Column(db.Integer, db.ForeignKey('BudgetHeading.id'), primary_key=True, autoincrement=1)
     budgetheading_id = db.Column(db.Integer, db.ForeignKey('budgetheading.id'))
     name = db.Column(db.String(255), Nullable=False)
@@ -15,35 +14,31 @@ class Expense(db.Model):
     invoices = db.relationship('Invoice', backref='expense',
                             lazy='dynamic')
     observations = db.Column(db.Text, Nullable=False)
-
-def __init__(self, name=None, school=None, expense_date=None, register_date=None, add_date=None, amount=None, revoked=False, invoices=[], observations=None):
-
-    self.name = name
-    self.school = school
-    self.expense_date = expense_date
-    self.register_date = register_date
-    self.add_date = add_date
-    self.amount = amount
-    if(amount >= 0):
-        self.rate = 1
-    else:
-        self.rate = -1
-    self.revoked = revoked
-    self.invoices = invoices
-    self.observations = observations
-
-def __repr__(self):
-
-    return{
-        'id': self.id,
-        'name': self.name,
-        'school': self.school,
-        'expense_date': self.expense_date,
-        'register_date': self.register_date,
-        'add_date': self.add_date,
-        'amount': self.amount,
-        'rate': self.rate,
-        'revoked': self.revoked,
-        'invoices': self.invoices,
-        'observations': self.observations
-    }
+    def __init__(self, name=None, school=None, expense_date=None, register_date=None, add_date=None, amount=None, revoked=False, invoices=[], observations=None):
+        self.name = name
+        self.school = school
+        self.expense_date = expense_date
+        self.register_date = register_date
+        self.add_date = add_date
+        self.amount = amount
+        if(amount >= 0):
+            self.rate = 1
+        else:
+            self.rate = -1
+        self.revoked = revoked
+        self.invoices = invoices
+        self.observations = observations
+    def __repr__(self):
+        return{
+            'id': self.id,
+            'name': self.name,
+            'school': self.school,
+            'expense_date': self.expense_date,
+            'register_date': self.register_date,
+            'add_date': self.add_date,
+            'amount': self.amount,
+            'rate': self.rate,
+            'revoked': self.revoked,
+            'invoices': self.invoices,
+            'observations': self.observations
+        }.__str__()
